ef76647b4a43ffbd3d11444a4a6d0cc1fc0c0361
BuildTools <unconfigured@null.spigotmc.org>
2020-10-11 15:46:44 +0200

    Points perso

--- commands/Popsauce/points.js
+++ commands/Popsauce/points.js
@@ -1,4 +1,5 @@
 module.exports.run = (client, message, args) => {
+  var mod = require('../../lists');//importe les variables relatives au jeu depuis le fichier main.js.
   mod.points = args;
   const joinstart = client.channels.cache.find(channel => channel.name === 'join-start');//l'id du salon salon nomé join-start es stocké dans la variable joinstart.
   joinstart.send("Nombre de points nécéssaires définit à "+args+" points.");
